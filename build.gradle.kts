7fbf76056731184df0682e5dc2b6189724ee00d8
Joshua Elsasser <joshua@elsasser.org>
2023-02-10 11:47:24 -0700

    Teach versionCatalogUpdate task to only update to stable releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
-@file:Suppress("DSL_SCOPE_VIOLATION") // XXX https://youtrack.jetbrains.com/issue/KTIJ-19369
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 subprojects {
     group = "net.joshe.mcmapper"
     version = "0.1"
 }
 
+@Suppress("DSL_SCOPE_VIOLATION") // XXX https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     alias(libs.plugins.benmanes.versions)
     alias(libs.plugins.littlerobots.versioncatalogupdate)
@@ -21,4 +22,19 @@ plugins {
             alias(libs.plugins.compose).apply(false)
         }
     }
+}
+
+// From https://github.com/ben-manes/gradle-versions-plugin#rejectversionsif-and-componentselection
+// via https://github.com/littlerobots/version-catalog-update-plugin#creating-a-libsversionstoml-file
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+  rejectVersionIf {
+    isNonStable(candidate.version) && !isNonStable(currentVersion)
+  }
 }
